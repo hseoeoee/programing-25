37464e49d867464a3ff22ca93b59f83b6cb285b2
hseoeoee <hseoeo00@gmail.com>
2025-04-08 14:51:29 +0900

    dkssud

---
+++ lecture04/inst.c
@@ -0,0 +1,25 @@
+﻿
+int main()
+{
+        int i;
+        int j;
+	i=3;
+	j=1;
+       
+
+
+
+	while(i)
+        {
+                puts ("oo");
+		i=i-1;
+	}
+        while(j)
+        {
+                puts ("dd");
+                j=j-1;
+        }
+
+
+	return 0;
+}
---
+++ lecture04/main.c
@@ -0,0 +1,16 @@
+int title_screen()
+{
+	puts("********************");
+	puts("***** p'love********");
+	puts("********************");
+	return 0;
+}	
+int main()
+	title_screen();
+	title_screen();
+	title_screen();
+	title_screen();
+        title_screen();
+	return 0;
+}
+
